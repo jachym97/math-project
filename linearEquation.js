0c24aee9f1e45a93132fadb41d08277df2e72e0f
jachym97 <135002816+jachym97@users.noreply.github.com>
2025-03-02 12:12:54 +0100

    Update linearEquation.js

--- linearEquation.js
+++ linearEquation.js
@@ -12,13 +12,13 @@ class LinearEquation extends Equation {
         this.c = c;
     }
 
-    GenerateEquation() {
+    GenerateEquation() { // vygeneruje lineární rovnici
         const variableX = 'x';
         const space = ' ';
         const equalsSign = '=';
-        this.Sign  = super.GetRandomSign();
+        this.Sign  = super.GetRandomSign(); // náhodná znaménka do rovnice
         this.Sign2 = super.GetRandomSign(); 
-        this.a = super.GetCoefficient(1,10);
+        this.a = super.GetCoefficient(1,10); // náhodné koeficienty do rovnice
         this.b = super.GetCoefficient(1,10);
         this.c = super.GetCoefficient(1,10);
     
@@ -32,7 +32,7 @@ class LinearEquation extends Equation {
         return this.Equation;
     }
 
-    SolveEquation() {
+    SolveEquation() { // vrátí řešení rovnice v závislosti na dvojici vygenerovaných znamének 
         if (this.Sign === '+' && this.Sign2 === '+') {
             this.EqSolution = (this.c - this.b) / this.a;
         } 
@@ -49,25 +49,25 @@ class LinearEquation extends Equation {
         return this.EqSolution;
     }
 
-    CompareSolutions() {
+    CompareSolutions() { // porovná výsledek zadaný uživatelem se správným výsledkem rovnice
         const inputSolution = parseFloat(document.getElementById("LinSolution").value);
         this.Solution = this.SolveEquation();
         
-        if (document.getElementById('RoundingCheckbox').checked) {
+        if (document.getElementById('RoundingCheckbox').checked) { // pokud je zaškrtnutý checkbox, tak se výsledek zaokrouhlí na dvě desetinná čísla
            this.Solution = super.RoundToDecimals(this.Solution, 2);
         }
-        if (inputSolution === this.Solution) {
+        if (inputSolution === this.Solution) { // pokud se hodnota zadaná uživatelem rovná řešení rovnice
             document.getElementById("LinEqMessage").innerText = "Správné řešení!";
         } else {
             document.getElementById("LinEqMessage").innerText = "Nesprávné řešení!";
         }
     }
 
-    ShowEquation() {
+    ShowEquation() { // zobrazí rovnici
         document.getElementById("generatedLinEq").innerText = this.GenerateEquation();
     }
 
-    ShowSolution() {
+    ShowSolution() { // zobrazí řešení rovnice
         document.getElementById("LinEqSolution").innerText = "Řešení: " + this.Solution;
     }
 }
